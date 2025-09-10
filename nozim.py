87e1500e80b615929d871f8deb2a4440364e6a5d
Nozimjon00001 <nozimjonrustamov17@gmail.com>
2025-09-10 21:44:06 +0500

    yuklash

---
+++ nozim.py
@@ -0,0 +1,88 @@
+print("Python kodlar")
+
+print()
+print()
+ 
+print("print()")
+print()
+print("'Assalomu aleykum'")
+print()
+print('Hayrli tong!')
+print()
+print(2+4*2)
+print()
+print(19/5)
+print()
+print(2**4)
+
+print()
+print()
+
+print("Arifmetik amallar")
+
+print()
+print()
+
+print("""Nexia", "Tico", 'Damas' ko'rganlar qilar havas""")
+print()
+print("5 ning 4-darajasini toping ""5**4 =",5**4)
+print()
+print("22 ni 4 ga bo'lganda qancha qoldiq qoladi? ""22%4 =",22%4)
+print()
+print("Tomonlari 125 ga teng kvadratning yuzi va primetrini toping ""yuzi 125*125 =",125**2,"primetri esa 125*2 =",125*2)
+print()
+print("Diametri 12 ga teng bo'lgan doiraning yuzini toping(pi=3.14 deb oling)""yuzi 12*pi =",12*3.14)
+print()
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping ""c**2 = 6**2+7**2 =",6**2+7**2)
+
+print()
+print()
+
+print("O'zgaruvchilar ")
+print()
+salom = '"Hello World!"'
+print(salom)
+print()
+xabar = "salom mening ismim Nozimjon men hacker bo'lmoqchiman "
+print(xabar)
+print()
+xabar = "fikrim o'zgardi dasturchi bo'lmoqchiman"
+print(xabar)
+print()
+# class = "albatta hammasi yaxshi bo'ladi chunki harakatimizni olloh ko'rib turibdi"
+print()
+
+print("str string matn")
+
+print()
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
