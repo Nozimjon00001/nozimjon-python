941b8b4595afe04ce3f1aec49e0abaa0e8d0616d
Nozimjon00001 <nozimjonrustamov17@gmail.com>
2025-09-11 21:11:08 +0500

    #05 STRING (MATN)

---
+++ #05 STRING (MATN).py
@@ -0,0 +1,17 @@
+print("#05 SITRING (MATN)\n")
+
+kocha = "Bog'bon"
+mahalla = "Sog'bon"
+tuman = "Bodomzor"
+viloyat = "Samarqand"
+
+print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati\n")
+
+kocha = input("Ko'changizni kiriting: ")
+mahalla = input("mahallangizni ham ")
+tuman = input("tumaningizni ham ")
+viloyat = input("viloyatingizni ham ")
+
+print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati\n")
+
+manzil = f"{kocha} {mahalla} {tuman} {viloyat}"
