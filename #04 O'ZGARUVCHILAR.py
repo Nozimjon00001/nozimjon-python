3d92a90c15f3a96c836f25db321668fbfcd0bf4e
Nozimjon00001 <nozimjonrustamov17@gmail.com>
2025-09-11 13:07:08 +0500

    #04 O'ZGARUVCHILAR

---
+++ #04 O'ZGARUVCHILAR.py
@@ -0,0 +1,18 @@
+print("#04 O'ZGARUVCHILAR")
+
+print()
+print()
+
+salom = '"Hello World!"'
+print(salom)
+print()
+xabar = "Mening ismim Nozimjon"
+print(xabar)
+print()
+xabar = "Mening familiyam Rustamov"
+print(xabar)
+print()
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylanining yuzi =", aylana_yuzi)
