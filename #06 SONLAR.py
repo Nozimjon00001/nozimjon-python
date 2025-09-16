20120e7e6275a868c7f2c2a50565ee85226caa84
Nozimjon00001 <nozimjonrustamov17@gmail.com>
2025-09-16 17:09:33 +0500

    #06 SONLAR

---
+++ #06 SONLAR.py
@@ -0,0 +1,18 @@
+print("#06 SONLAR\n\n")
+
+son = int(input("Biror son kiriting:\n>>> "))
+kvadrat = son**2
+kub = son**3
+print(f"{son} ning kvadrati {kvadrat} ga teng")
+print(f"{son} ning kubi {kub} ga teng\n")
+
+yosh = int(input("Yoshingiz nechida?\n>>>"))
+yil = 2025-yosh
+print(f"Siz {yil} da tug'ulgansiz\n")
+
+a = float(input("Birinchi sonni kiriting:"))
+b = float(input("Ikkinchi sonni kiriting:"))
+print("a+b =",a+b)
+print("a-b =",a-b)
+print("a*b =",a*b)
+print("a/b =",a/b)
