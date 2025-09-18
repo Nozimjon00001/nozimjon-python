cc831a9a3960a6516aa394af6a62761c48569d3f
Nozimjon00001 <nozimjonrustamov17@gmail.com>
2025-09-18 19:11:43 +0500

    #08 RO'YXATLAR BILAN ISHLASH

---
+++ #08 RO'YXATLAR BILAN ISHLASH.py
@@ -0,0 +1,25 @@
+print("#08 RO'YXATLAR BILAN ISHLASH\n")
+
+davlatlar = ["O'zbekiston","Turkiston","Yaponiya","Germaniya"]
+print(len(davlatlar))
+
+print(sorted(davlatlar))
+
+print(sorted(davlatlar, reverse=True))
+
+print(davlatlar)
+
+print(davlatlar.reverse())
+
+print(davlatlar.sort())
+
+print(davlatlar.sort(reverse=True))
+
+sonlar = list(range(120,1200,2))
+print(sonlar)
+
+print(sum(sonlar))
+
+print(max(sonlar)-min(sonlar))
+
+print(len(sonlar))
