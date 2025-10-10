e9799b09119e42a63e37a299cc3ebc373aacd82e
Nozimjon00001 <nozimjonrustamov17@gmail.com>
2025-10-10 14:58:49 +0500

    fhgfgsgdggf

---
+++ jfskjfdjfkdj1.py
@@ -0,0 +1,4 @@
+def toliq_ism_yasa(ism, familiya):
+    """Toliq isma qaytaruvchi funksiya"""
+    toliq_ism = f"{ism} {familiya}"
+    return toliq_ism # qiymat qaytarish uchun return operatorini ishlatamiz
