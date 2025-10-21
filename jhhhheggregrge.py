7a22b02f1f931587fa883602a1bee8c6c5067b3d
Nozimjon00001 <nozimjonrustamov17@gmail.com>
2025-10-21 09:25:03 +0500

    jsjdhfefhe

---
+++ jhhhheggregrge.py
@@ -0,0 +1,29 @@
+# juft = input("iltimos juft son kiriting: ")
+# juft = int(juft)
+# if juft%2 == 0:
+#     print("raxmat")
+# else:
+#     print("bu juft emas")
+
+# yosh = input("yoshingiz nechida: ")
+# yosh = int(yosh)
+# if yosh < 4 or yosh > 60:
+#     pul = "bepul"
+# elif yosh < 18:
+#     pul = "10000so'm"
+# elif yosh > 18:
+#     pul = "18000"
+# print(pul)
+
+
+# x = input("son kiriting: ")
+# y = input("son kiriting: ")
+# x = int(x)
+# y = int(y)
+# if x > y:
+#     print(f"{x}>{y}")
+# elif x == y:
+#     print(f"{x}={y}")
+# else:
+#     print(f"{x}<{y}")
+
